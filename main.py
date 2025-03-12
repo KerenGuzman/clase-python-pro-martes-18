4670f8402f42abed8c8f858e1b6a696b65821fc7
KerenGuzman <91639005+KerenGuzman@users.noreply.github.com>
2025-03-11 19:05:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import random
-elements = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-pass_length = int(input("Introduzca la longitud del pase: "))
+import discord
+from discord.ext import commands
 
-password = ""
-for i in range(pass_length):
-    password += random.choice(elements)
+intents = discord.Intents.default()
+intents.message_content = True
 
-print(password)
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def llamame(ctx):
+    await ctx.send(f'Hola, soy una funcion!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("")
